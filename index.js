cfa89064be1207380c511d72c0895006aa59c359
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-07 18:44:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ function add(a,b) {
 function subtract(a,b) {
   return a-b
 } 
+
+function multiply(a,b) {
+  return a*b
+}
+
+function divide(a,b) {
+  return a/b
+}
+
 function makeInt(n) {
   
 }
